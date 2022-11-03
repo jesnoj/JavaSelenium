dbd4a423911facad05941529db6cf587ebdc7bf7
Jesno Joseph <jesnojextc220@siesgst.ac.in>
2022-11-03 18:34:17 +0530

    second commit

--- project_1/src/package1/DayFour.java
+++ project_1/src/package1/DayFour.java
@@ -1,5 +1,27 @@
 package package1;
 
 public class DayFour {
+	public DayFour(int a, int b,int c)
+	{
+		System.out.println("3 paramatrized");
+	}
+	public DayFour()
+	{
+	this(3,6,5);
+		System.out.println("default");
+	}
+	public DayFour(int a,int b)
+	{
+		this();
+		System.out.println("2 paramatrized");
+	}
+	public DayFour(int a)
+	{
+		this(3,3);
+		System.out.println("1 parametrised");
+	}
 
+	public static void main(String[] args) {
+		DayFour abc=new DayFour(454);
+	}
 }
